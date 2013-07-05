0c3e5b0dd80727ac468bb1c1e24506e029a49c94
Jordan <jordan@woelfware.com>
2013-07-04 19:24:10 -0500

    added setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Simple setup.sh for configuring rpm based linux setup
+
+sudo yum groupinstall \"development\"
+sudo yum groupinstall \"development-tools\"
+sudo yum install npm
+
+# Install jshint to allow checking of JS code 
+# http://jshint.com/
+npm install -g jshint
+
+#ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.vimrc .
+ln -sb dotfiles/.gvimrc .
+ln -sb dotfiles/.vim .
