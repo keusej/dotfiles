b5ad4669d6b4ddd8d236aa99fe8d2e7f0a9c2b4e
Jordan <jordan@woelfware.com>
2013-07-07 13:34:16 -0500

    setup.sh

--- setup.sh
+++ setup.sh
@@ -18,3 +18,9 @@ ln -sb dotfiles/.vim .
 
 #useful tools to install
 sudo yum install ack
+
+# TODO add more vim bundles
+git clone https://github.com/tpope/vim-surround.git dotfiles/.vim/bundle/ 
+git clone https://github.com/godlygeek/tabular.git dotfiles/.vim/bundle/ 
+git clone https://github.com/tpope/vim-fugitive.git dotfiles/.vim/bundle/ 
+git clone https://github.com/tpope/vim-sensible.git dotfiles/.vim/bundle/ 
