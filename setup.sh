468bee7f628be9c5ea69ccaf3bee634339f1e76d
Jordan <jordan@woelfware.com>
2013-07-17 07:30:18 -0500

    added more vim bundles

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 
 sudo yum groupinstall \"development\"
 sudo yum groupinstall \"development-tools\"
+
+#useful tools to install
+sudo yum install ack
 sudo yum install npm
 
 # Install jshint to allow checking of JS code 
@@ -16,11 +19,10 @@ ln -sb dotfiles/.vimrc .
 ln -sb dotfiles/.gvimrc .
 ln -sb dotfiles/.vim .
 
-#useful tools to install
-sudo yum install ack
-
 # TODO add more vim bundles
-git clone https://github.com/tpope/vim-surround.git dotfiles/.vim/bundle/ 
-git clone https://github.com/godlygeek/tabular.git dotfiles/.vim/bundle/ 
-git clone https://github.com/tpope/vim-fugitive.git dotfiles/.vim/bundle/ 
-git clone https://github.com/tpope/vim-sensible.git dotfiles/.vim/bundle/ 
+git clone https://github.com/tpope/vim-surround.git .vim/bundle/ 
+git clone https://github.com/godlygeek/tabular.git .vim/bundle/ 
+git clone https://github.com/tpope/vim-fugitive.git .vim/bundle/ 
+git clone https://github.com/tpope/vim-sensible.git .vim/bundle/ 
+git clone https://github.com/kien/rainbow_parentheses.vim.git .vim/bundle/
+git clone https://github.com/scrooloose/nerdtree.git .vim/bundle/
