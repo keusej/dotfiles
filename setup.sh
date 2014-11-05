3cd691f8ccf488daf6c082b27133b2ca375d2af1
Jordan <j.keuseman@gmail.com>
2014-11-04 20:57:51 -0600

    added solarized

--- setup.sh
+++ setup.sh
@@ -21,10 +21,10 @@ ln -sb dotfiles/.vimrc .
 ln -sb dotfiles/.gvimrc .
 ln -sb dotfiles/.vim .
 
-# TODO add more vim bundles
 git clone https://github.com/tpope/vim-surround.git .vim/bundle/vim-surround 
 git clone https://github.com/godlygeek/tabular.git .vim/bundle/tabular
 git clone https://github.com/tpope/vim-fugitive.git .vim/bundle/vim-fugitive
 git clone https://github.com/tpope/vim-sensible.git .vim/bundle/vim-sensible
+git clone git://github.com/altercation/vim-colors-solarized.git .vim/bundle/solarized
 git clone https://github.com/kien/rainbow_parentheses.vim.git .vim/bundle/rainbow_parenthesis.vim
 git clone https://github.com/scrooloose/nerdtree.git .vim/bundle/nerdtree
